9ae56a47dc4edd8b74a53c69da62fc11d1918af4
Your Name <you@example.com>
2019-03-15 12:58:28 -0700

    create base and about templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
 
-def home(request):
-    return render(request, about.html)
+def about(request):
+    return render(request, 'about.html')
+    # return HttpResponse('about page')
