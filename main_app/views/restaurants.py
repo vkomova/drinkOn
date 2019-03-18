c4cf4ce2ef0f2203d460a268985e1a06201d5f7d
Your Name <you@example.com>
2019-03-18 14:29:04 -0700

    implement add and view restaurant functionality

--- main_app/views/restaurants.py
+++ main_app/views/restaurants.py
@@ -1,25 +1,19 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView
 from ..models import Restaurant, Menu, MenuVote, Hours, HoursVote
 
-def display_restaurant(request):
-    restaurant = Restaurant.objects.find(google_assigned_id=request.google_assigned_id)
-    return render(request, f'restaurants/{restaurant.id}/view.html', 'restaurant': restaurant)
-
-class CreateRestaurant(CreateView):
-    model = Restaurant
-    fields = '__all__'
-
 def check_restaurant(request):
-    if Restaurant.objects.filter(google_assigned_id=request.google_assigned_id).exists():
-        # restaurant = Restaurant.objects.find(google_assigned_id=request.google_assigned_id)
-        # return render(request, f'restaurants/{restaurant.id}/view.html')
-        # display restaurant function
-        display_restaurant(request)
+    # print(request.POST)
+    print(request.POST['google_place_id'])
+    if Restaurant.objects.filter(google_place_id=request.POST['google_place_id']).exists():
+        print('exists')
+        restaurant = Restaurant.objects.get(google_place_id=request.POST['google_place_id'])
+        return render(request, 'restaurants/details.html', {'restaurant': restaurant})
     else:
-        # Restaurant.objects.create(request)
-        # create model
-        CreateRestaurant.as_view(request)
+        restaurant = Restaurant.objects.create(name=request.POST['name'], address=request.POST['address'], google_place_id=request.POST['google_place_id'])
+        print(restaurant)
+        return render(request, 'restaurants/details.html', {'restaurant': restaurant})
